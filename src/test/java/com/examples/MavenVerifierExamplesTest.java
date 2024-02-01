44a049a2fbbd026f6b55a7910b2d7dff9d2f2867
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2024-02-01 12:29:16 +0100

    first test

---
+++ src/test/java/com/examples/MavenVerifierExamplesTest.java
@@ -0,0 +1,20 @@
+package com.examples;
+
+import java.io.File;
+
+import org.apache.maven.shared.verifier.VerificationException;
+import org.apache.maven.shared.verifier.Verifier;
+import org.junit.Test;
+
+public class MavenVerifierExamplesTest {
+
+	@Test
+	public void testRunOnMavenQuickStartExample() throws VerificationException {
+		String baseDir = new File("target/test-classes/maven-quickstart-example").getAbsolutePath();
+		Verifier verifier = new Verifier(baseDir);
+		verifier.addCliArgument("package");
+		verifier.execute();
+		verifier.verify(true); // throws an exception in case of errors in the build log
+		verifier.verifyFilePresent(verifier.getBasedir() + "/target/maven-quickstart-example-1.0-SNAPSHOT.jar");
+	}
+}
